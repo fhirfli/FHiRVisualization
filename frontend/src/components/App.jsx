92b875f31934fa6e22b20ae4d7562f1e8a47f480
AlexBO97 <alexandru_bondor@yahoo.com>
2017-11-21 18:56:26 +0000

    .

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { render } from 'react-dom';
+import { VictoryPie } from 'victory';
+
+class PieChart extends Component {
+    render() {
+      return (
+        <VictoryPie />
+      );
+    }
+  }
+  
 
 export default class App extends Component {
     render() {
         return (
-            <div className="basic">
-                Hello not quite from react with or without HMRE! actually now it has HRME, actually it does. But maybe it does? Nope, oh wait it does - does it?
-            </div>
+            <PieChart/>
         );
     }
 }
