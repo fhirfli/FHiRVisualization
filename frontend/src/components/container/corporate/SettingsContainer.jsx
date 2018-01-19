ece64b46cff8e89cbe93c678dd563a6f29a22301
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-19 21:03:55 +0000

    implemented corporate settings page

--- frontend/src/components/container/corporate/SettingsContainer.jsx
+++ frontend/src/components/container/corporate/SettingsContainer.jsx
@@ -1,10 +1,17 @@
 import {connect} from "react-redux";
 import Settings from "components/pure/corporate/Settings";
+import * as corporateSettingsActions from "actions/corporate/settings";
 
 const mapStateToProps = (state) => {
-    return {};
+    return {
+        users: state.corporate.settings.users,
+        hasErrored: state.corporate.settings.hasErrored,
+        isLoading: state.corporate.settings.isLoading,
+        errorMsg: state.corporate.settings.errorMsg
+    };
 };
 
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    corporateSettingsActions
 )(Settings);
