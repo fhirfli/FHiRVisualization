91226263f33d2a0dbd3845a144f108924fb3f12b
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-19 19:39:19 +0000

    implemented home page basic data display

--- frontend/src/components/pure/individual/Home.jsx
+++ frontend/src/components/pure/individual/Home.jsx
@@ -2,29 +2,56 @@ import React, {Component} from "react";
 import {VictoryLine, VictoryChart, VictoryTheme} from 'victory';
 import '../styles/Home.scss';
 import moment from 'moment';
+import * as propTypes from 'prop-types';
 
-const elem = () => (
-    <div style={{
-        backgroundColor: 'red',
+const elem = (colour, string) => (
+    <div key={string} style={{
+        backgroundColor: {colour},
         width: 100,
         height: 100
     }}>
-        Data
+        {string}
     </div>
 );
 
 export default class Home extends Component {
+    componentDidMount() {
+        this.props.manualLoadGoals();
+        this.props.manualLoadPreferences();
+    }
     render() {
         return (
             <div id="home-container">
                 <div id="home-content">
                     <h2>Home</h2>
                     <h4>{moment().format("ddd D MMMM")}</h4>
-                    {elem()}
-                    {elem()}
-                    {elem()}
+                    {
+                        this.props.preferences.map((preference) => {
+                            /*                                     "dataType": "HeartRate",
+                             "colour":  DEFAULT_COLOUR,
+                             "visualization": [],
+                             */
+                            return preference.visualization.map((visualization, i) => {
+                                return (elem(preference.colour, visualization + " " + preference.dataType + " " + i));
+                            });
+                        })
+                    }
+                    {
+                        this.props.goals.map((goal, i) => {
+                            return (elem(goal.colour, goal.name + " " + goal.value + " " + i))
+                        })
+                    }
                 </div>
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    manualLoadGoals: propTypes.func.isRequired,
+    manualLoadPreferences: propTypes.func.isRequired,
+    manualLoadData: propTypes.func.isRequired,
+    preferences: propTypes.array.isRequired,
+    goals: propTypes.array.isRequired,
+    data: propTypes.object.isRequired
+};
--- frontend/src/reducers/individual/home.js
+++ frontend/src/reducers/individual/home.js
@@ -1,6 +1,19 @@
 import {} from "constants/individual/home";
 
-const home = (state = {}, action) => {
+const home = (state = {
+    data: {
+        /*
+         // only the datasets requested will be present
+         'heartRate': {
+         // only the ranges requested will be present
+         'Annual': [{<FHIR-PROFILE>},{<FHIR-PROFILE>}....]
+         'Daily': [{<FHIR-PROFILE>},{<FHIR-PROFILE>}....]
+         'Weekly': [{<FHIR-PROFILE>},{<FHIR-PROFILE>}....]
+         }
+
+         */
+    }
+}, action) => {
     switch (action.type) {
         default:
             // VERY IMPORTANT
