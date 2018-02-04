55b186c3d4ee8c5ae760f259e800478d881aa487
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-02-04 20:49:21 +0000

    implemented observation profile

---
+++ backend/db/models/FHiR/Observation.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+let snowmed = require('./SnowmedCodes');
+
+const observationSchema = new Schema({
+    // identifier is implicitly present
+    status: {type: String, enum: ["registered", "preliminary", "final", "amended"], required: true},
+    category: {
+        coding: {
+            snowmedCT: {type: String, enum: snowmed.snowmedCategoryCodes}
+        }
+    },
+    code: {
+        coding: {
+            snowmedCT: {type: String, enum: snowmed.snowmedCTCodes, required: true},
+            required: true
+        }
+    },
+    subject: {
+        type: ObjectId, ref: 'IndividualUser', required: true
+    },
+    effective: {
+        type: mongoose.Schema.Types.Date,
+        required: true
+    },
+    issued: {
+        type: mongoose.Schema.Types.Date,
+        required: true
+    },
+    performer: {
+        type: ObjectId,
+        ref: 'Organization',
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String
+    },
+    bodySite: {
+        coding: {
+            snowmedCT: {type: String, enum: snowmed.snowmedBodySiteCodes}
+        }
+    },
+    method: {
+        coding: {
+            snowmedCT: {type: String, enum: snowmed.snowmedMethodCodes}
+        },
+        text: {type: String}
+    },
+    device: {
+        type: String
+    }
+});
+
+
+const Observation = mongoose.model('Observation', observationSchema);
+module.exports = Observation;
---
+++ backend/db/models/FHiR/SnowmedCodes.js
@@ -0,0 +1,16 @@
+module.exports = {
+    snowmedCTCodes: [
+        // include allowable snowmedCodes here
+        "1011CT"
+    ],
+    snowmedCategoryCodes: [
+        "1011CT"
+    ],
+
+    snowmedBodySiteCodes: [
+        "1011CT"
+    ],
+    snowmedMethodCodes: [
+        "1011CT"
+    ]
+};
