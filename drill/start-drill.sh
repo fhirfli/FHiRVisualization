cd966eadddeb98ceb5b0873634b2a85d47c23690
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-14 18:14:58 +0000

    removed unnecassary file

--- drill/start-drill.sh
+++
@@ -1,4 +0,0 @@
-sed -r -i "s/(zk.connect):(.*)/\1: \"$ZK\"/g" /opt/drill/conf/drill-override.conf
-sed -r -i "s/(cluster-id):(.*)/\1: \"$CLUSTER_ID\"/g" /opt/drill/conf/drill-override.conf
-
-/opt/drill/bin/drillbit.sh --config /opt/drill/conf/ start
