62e6a43210c51952259012959f915a079905f22c
AlexBO97 <alexandru_bondor@yahoo.com>
2018-02-19 13:12:34 +0000

    tests

--- test/test_login.js
+++ test/test_login.js
@@ -6,6 +6,8 @@ const expect = chai.expect;
 // chai.use(chaiAsPromised);
 // chai.should();
 
+const base_site = "http://localhost:37832";
+
 const options = {
     desiredCapabilities: {
         browserName: 'chrome'
@@ -18,7 +20,7 @@ describe('Individual Login', function () {
     this.timeout(500000);
     before(() => {
 
-        }
+    }
     );
 
     after(() => {
@@ -29,15 +31,69 @@ describe('Individual Login', function () {
         browser = webdriverio
             .remote(options)
             .init()
-            .url('http://localhost:8000/individual/home')
+            .url(base_site + '/individual/home')
             .getTitle()
             .then(function (title) {
                 console.log(title);
-                expect(title).t0.equal('FHiR Visualizations');
+                expect(title).to.equal('FHiR Visualization');
                 done();
             })
-            .end();
+            .end()
+            .catch(done);
+    });
+
+    it('should accept writing username and password', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/individual/home')
+            .setValue("//*[@id=\"login-form\"]/input[1]", "alex@gmail.com")
+            .getValue("//*[@id=\"login-form\"]/input[1]")
+            .then(function (value) {
+                console.log(value)
+                expect(value).to.equal("alex@gmail.com")
+            })
+            .setValue("//*[@id=\"login-form\"]/input[2]", "password")
+            .getValue("//*[@id=\"login-form\"]/input[2]")
+            .then(function (value) {
+                console.log(value)
+                expect(value).to.equal("password")
+                done();
+            })
+            .end()
+            .catch(done);
+    });
+
+    it('should specify which login page it is', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/individual/home')
+            .getText("//*[@id=\"text\"]/p")
+            .then(function (title) {
+                console.log(title);
+                expect(title).to.equal('Individual Account');
+                done();
+            })
+            .end()
+            .catch(done);
+    });
+
+    it('should allow people to log in', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/individual/home')
+            .setValue("input[type=\"email\"]", "alex@gmail.com")
+            .setValue("input[type=\"password\"]", "password")
+            .submitForm("#login-form")
+            .waitForVisible("//*[@id=\"home-content__header\"]/h2")
+            .getUrl()
+            .then(function (value) {
+                expect(value).to.equal("http://localhost:37832/individual/home")
+                done();
+            })
+            .end()
+            .catch(done);
     });
 });
-//
-
