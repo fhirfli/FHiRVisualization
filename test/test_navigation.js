eeda44abb1baba2884471e65c92882d328e238b2
AlexBO97 <alexandru_bondor@yahoo.com>
2018-02-20 13:07:39 +0000

    corporate navigation

--- test/test_navigation.js
+++ test/test_navigation.js
@@ -108,3 +108,76 @@ describe('Individual Navigation', function () {
     });
 
 });
+
+describe('Corporate Navigation', function () {
+    this.timeout(500000);
+    before(() => {
+
+    }
+    );
+
+    after(() => {
+        browser.end();
+    });
+
+    it('should allow navigating to data', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/corporate/home')
+            .setValue("input[type=\"email\"]", "company@gmail.com")
+            .setValue("input[type=\"password\"]", "password")
+            .submitForm("#login-form")
+            .pause(100)
+            .click("//*[@id=\"navbar-options-container\"]/ul/li[2]/a")
+            .waitForVisible("//*[@id=\"data-title\"]/h2")
+            .getUrl()
+            .then(function (value) {
+                expect(value).to.equal("http://localhost:37832/corporate/data")
+                done();
+            })
+            .end()
+            .catch(done);
+    });
+
+    it('should allow navigating to settings', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/corporate/home')
+            .setValue("input[type=\"email\"]", "company@gmail.com")
+            .setValue("input[type=\"password\"]", "password")
+            .submitForm("#login-form")
+            .pause(100)
+            .click("//*[@id=\"navbar-options-container\"]/ul/li[3]/a")
+            .waitForVisible("//*[@id=\"settings-title\"]/h2")
+            .getUrl()
+            .then(function (value) {
+                expect(value).to.equal("http://localhost:37832/corporate/settings")
+                done();
+            })
+            .end()
+            .catch(done);
+    });
+
+    it('should allow logging out', function (done) {
+        browser = webdriverio
+            .remote(options)
+            .init()
+            .url(base_site + '/corporate/home')
+            .setValue("input[type=\"email\"]", "company@gmail.com")
+            .setValue("input[type=\"password\"]", "password")
+            .submitForm("#login-form")
+            .pause(100)
+            .click("//*[@id=\"navbar-auth-button\"]")
+            .waitForVisible("//*[@id=\"about-content\"]/div[1]/h2")
+            .getUrl()
+            .then(function (value) {
+                expect(value).to.equal("http://localhost:37832/")
+                done();
+            })
+            .end()
+            .catch(done);
+    });
+
+});
